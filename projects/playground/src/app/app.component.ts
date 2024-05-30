16951e20aba1dc4e82c4c127b61443331be86fb1
glapoint22 <glapoint22@gmail.com>
2024-05-30 09:45:43 -0400

    Completed themes

--- projects/playground/src/app/app.component.ts
+++ projects/playground/src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Renderer2, inject } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { IconComponent, RadioButtonComponent } from '../../../ngx-components/src/public-api';
 import { RadioGroupComponent } from '../../../ngx-components/src/lib/components/radio-group/radio-group.component';
@@ -20,7 +20,28 @@ import { CheckboxComponent } from '../../../ngx-components/src/lib/components/ch
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
+  protected buttonLabel!: string;
+  private renderer = inject(Renderer2);
+
   favoriteSeason: string = 'Summer';
   seasons: string[] = ['Winter', 'Spring', 'Summer', 'Autumn'];
   checked = false;
+
+  ngOnInit() {
+    this.renderer.addClass(document.body, 'light-theme');
+    this.buttonLabel = 'Dark Theme';
+  }
+
+
+  protected onClick(): void {
+    if (this.buttonLabel === 'Dark Theme') {
+      this.buttonLabel = 'Light Theme';
+      this.renderer.addClass(document.body, 'dark-theme');
+      this.renderer.removeClass(document.body, 'light-theme');
+    } else {
+      this.buttonLabel = 'Dark Theme';
+      this.renderer.addClass(document.body, 'light-theme');
+      this.renderer.removeClass(document.body, 'dark-theme');
+    }
+  }
 }
