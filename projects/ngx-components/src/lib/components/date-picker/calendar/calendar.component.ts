1e2657abb7d96449eb5c36b1f37f0a0fb300c18b
Gabe LaPoint <glapoint22@gmail.com>
2024-06-08 22:20:28 -0400

    Working on calendar views

--- projects/ngx-components/src/lib/components/date-picker/calendar/calendar.component.ts
+++ projects/ngx-components/src/lib/components/date-picker/calendar/calendar.component.ts
@@ -1,16 +1,60 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 import { DayViewComponent } from './day-view/day-view.component';
 import { MonthViewComponent } from './month-view/month-view.component';
 import { YearViewComponent } from './year-view/year-view.component';
 import { IconComponent } from '../../icon/icon.component';
+import { DatePipe } from '@angular/common';
+import { CalendarView } from './calendar-view';
 
 @Component({
   selector: 'calendar',
   standalone: true,
-  imports: [DayViewComponent, MonthViewComponent, YearViewComponent, IconComponent],
+  imports: [DayViewComponent, MonthViewComponent, YearViewComponent, IconComponent, DatePipe],
   templateUrl: './calendar.component.html',
   styleUrl: './calendar.component.scss'
 })
 export class CalendarComponent {
+  protected date = signal<Date>(new Date());
+  protected selectedDate = signal<Date>(new Date());
+  protected currentCalendarView = signal<CalendarView>(CalendarView.Day);
+  protected CalendarView = CalendarView;
 
+  public initializeDate(date: Date): void {
+    this.setDate(date);
+    this.setSelectedDate(date);
+  }
+
+  protected setDate(date: Date): void {
+    this.date.set(date);
+  }
+
+  protected setSelectedDate(date: Date): void {
+    this.selectedDate.set(date);
+  }
+
+  protected updateMonth(num: number): void {
+    const date = new Date(this.date().getFullYear(), this.date().getMonth() + num, 1);
+    this.setDate(date);
+    this.changeView(CalendarView.Day);
+  }
+
+  protected updateYear(num: number): void {
+    const date = new Date(this.date().getFullYear() + num, this.date().getMonth(), 1);
+    this.setDate(date);
+    this.changeView(CalendarView.Day);
+  }
+
+  protected updateDateToToday(): void {
+    this.setDate(new Date());
+    this.setSelectedDate(this.date());
+    this.changeView(CalendarView.Day);
+  }
+
+  protected changeView(view: CalendarView): void {
+    this.currentCalendarView.set(view);
+  }
+
+  protected onDateSelect(date: Date): void {
+    this.setSelectedDate(date);
+  }
 }
--- projects/ngx-components/src/lib/components/date-picker/calendar/day-view/day-view.component.ts
+++ projects/ngx-components/src/lib/components/date-picker/calendar/day-view/day-view.component.ts
@@ -1,5 +1,5 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, InputSignal, OnChanges, OutputEmitterRef, input, output } from '@angular/core';
 
 @Component({
   selector: 'day-view',
@@ -8,63 +8,83 @@ import { Component } from '@angular/core';
   templateUrl: './day-view.component.html',
   styleUrls: ['../calendar-grid.scss', './day-view.component.scss']
 })
-export class DayViewComponent {
-  protected calendarDays: any = [];
-  protected selectedDate: Date = new Date();
+export class DayViewComponent implements OnChanges {
+  public date: InputSignal<Date> = input.required<Date>();
+  public selectedDate: InputSignal<Date> = input.required<Date>();
+  public onDateSelect: OutputEmitterRef<Date> = output<Date>();
+  protected dates: Array<Date> = [];
+  protected currentMonth!: number;
 
-  ngOnInit() {
-    const currentDate = new Date();
-    const firstDayOfMonth = new Date(currentDate.getFullYear(), currentDate.getMonth(), 1);
+  public ngOnChanges(): void {
+    this.setCalendarDates();
+  }
+
+
+  private setCalendarDates(): void {
+    const date = this.date();
+    const firstDayOfMonth = new Date(date.getFullYear(), date.getMonth(), 1);
     const firstDayOfWeek = firstDayOfMonth.getDay(); // 0 (Sun) - 6 (Sat)
 
-    const daysInCurrentMonth = new Date(currentDate.getFullYear(), currentDate.getMonth() + 1, 0).getDate();
-    const daysInPreviousMonth = new Date(currentDate.getFullYear(), currentDate.getMonth(), 0).getDate();
+    const daysInCurrentMonth = new Date(date.getFullYear(), date.getMonth() + 1, 0).getDate();
+    const daysInPreviousMonth = new Date(date.getFullYear(), date.getMonth(), 0).getDate();
 
     const daysFromPrevMonth = firstDayOfWeek;
     const totalDays = 6 * 7; // 6 rows and 7 columns
     const daysFromNextMonth = totalDays - (daysFromPrevMonth + daysInCurrentMonth);
 
-    // Days from the previous month
+    this.dates = [];
+    this.currentMonth = date.getMonth();
+
+    // Dates from the previous month
+    this.setPreviousMonthDates(daysFromPrevMonth, daysInPreviousMonth, date);
+
+    // Dates from the current month
+    this.setCurrentMonthDates(daysInCurrentMonth, date);
+
+    // Dates from the next month
+    this.setNextMonthDates(daysFromNextMonth, date);
+  }
+
+
+
+  private setPreviousMonthDates(daysFromPrevMonth: number, daysInPreviousMonth: number, currentDate: Date): void {
     for (let i = daysFromPrevMonth; i > 0; i--) {
       const date = new Date(currentDate.getFullYear(), currentDate.getMonth() - 1, daysInPreviousMonth - i + 1);
 
-      this.calendarDays.push({
-        date: date,
-        day: date.getDate(),
-        isCurrentMonth: false,
-        isCurrentDay: this.isCurrentDay(date),
-        isSelectedDate: this.isSelectedDate(date)
-      });
-    }
-
-    // Days from the current month
-    for (let i = 1; i <= daysInCurrentMonth; i++) {
-      const date = new Date(currentDate.getFullYear(), currentDate.getMonth(), i);
-
-      this.calendarDays.push({
-        date: date,
-        day: date.getDate(),
-        isCurrentMonth: true,
-        isCurrentDay: this.isCurrentDay(date),
-        isSelectedDate: this.isSelectedDate(date)
-      });
-    }
-
-    // Days from the next month
-    for (let i = 1; i <= daysFromNextMonth; i++) {
-      const date = new Date(currentDate.getFullYear(), currentDate.getMonth() + 1, i);
-
-      this.calendarDays.push({
-        date: date,
-        day: date.getDate(),
-        isCurrentMonth: false,
-        isCurrentDay: this.isCurrentDay(date),
-        isSelectedDate: this.isSelectedDate(date)
-      });
+      this.dates.push(date);
     }
   }
 
-  private isCurrentDay(date: Date): boolean {
+
+
+
+  private setCurrentMonthDates(daysInCurrentMonth: number, currentDate: Date): void {
+    for (let i = 1; i <= daysInCurrentMonth; i++) {
+      const date = new Date(currentDate.getFullYear(), currentDate.getMonth(), i);
+
+      this.dates.push(date);
+    }
+  }
+
+
+
+
+  private setNextMonthDates(daysFromNextMonth: number, currentDate: Date): void {
+    for (let i = 1; i <= daysFromNextMonth; i++) {
+      const date = new Date(currentDate.getFullYear(), currentDate.getMonth() + 1, i);
+
+      this.dates.push(date);
+    }
+  }
+
+
+
+  protected isCurrentMonth(date: Date): boolean {
+    return date.getMonth() !== this.currentMonth;
+  }
+
+
+  protected isCurrentDay(date: Date): boolean {
     const todaysDate = new Date();
 
     return date.getDate() === todaysDate.getDate() &&
@@ -73,14 +93,16 @@ export class DayViewComponent {
   }
 
 
-  private isSelectedDate(date: Date): boolean {
-    return date.getDate() === this.selectedDate.getDate() &&
-      date.getMonth() === this.selectedDate.getMonth() &&
-      date.getFullYear() === this.selectedDate.getFullYear();
+
+  protected isSelectedDate(date: Date): boolean {
+    return date.getDate() === this.selectedDate().getDate() &&
+      date.getMonth() === this.selectedDate().getMonth() &&
+      date.getFullYear() === this.selectedDate().getFullYear();
   }
 
 
+
   protected onCalendarDayClick(date: Date): void {
-    this.selectedDate = date;
+    this.onDateSelect.emit(date);
   }
 }
